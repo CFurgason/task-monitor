81fa8309ca55df22c583e2560821de1899855e00
CFurgason <courtney@autoshopcallbacks.net>
2026-09-14 14:50:28 -0500

    Add files via upload

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 window.DASHBOARD_CONFIG = {
-  callsUrl: "https://docs.google.com/spreadsheets/d/1lmSyCCP0RnOhU2CWJv9LzfAaHe7R_y4D56Vr3CAJ3YA/gviz/tq?tqx=out:csv&gid=0",
-  togglesUrl: "",
+  callsUrl: "https://docs.google.com/spreadsheets/d/1-l0LXj6Mt7e73YdWcLGqRRLwR34GtrbnBWo6e-O3_qM/gviz/tq?tqx=out:csv&gid=0",
+  togglesUrl: "https://docs.google.com/spreadsheets/d/1QgVMPMMR4-dN1g3hqcw5BdgJl89Y8H-IUb6jdyqrYlM/gviz/tq?tqx=out:csv&gid=0",
   shopMappings: {
     "J.J.'s Auto Service Center": "JJ's Auto Service Center"
   },
